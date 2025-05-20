89730712aaaf0aefeeb14cdef99ca0abd82c4218
Yoshi-kisa <yoshifumi.kisanuki@mh-gr.com>
2025-05-20 17:52:45 +0900

    Add Supabase client4

--- src/supabaseclient.js
+++ src/supabaseclient.js
@@ -1,6 +1,6 @@
+// src/supabaseClient.js
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://hipmiuhyvdaqlaxbnwxx.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhpcG1pdWh5dmRhcWxheGJud3h4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc3MjUzNzMsImV4cCI6MjA2MzMwMTM3M30.dokjX1p7YSZGPwouJEukDlxOVWEs2_woY6OIvw6V8jM';
-
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhpcG1pdWh5dmRhcWxheGJud3h4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc3MjUzNzMsImV4cCI6MjA2MzMwMTM3M30.dokjX1p7YSZGPwouJEukDlxOVWEs2_woY6OIvw6V8jM'; // フルのAPIキーをここに
 export const supabase = createClient(supabaseUrl, supabaseKey);
